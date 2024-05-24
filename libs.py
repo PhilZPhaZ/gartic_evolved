c79b09fb15ae80bf1fd080e959d3d48be715c790
PhilZPhaZ <bour.phileas@gmail.com>
2024-05-24 17:04:01 +0200

    optimisations

--- libs.py
+++ libs.py
@@ -5,17 +5,14 @@ from io import BytesIO
 
 def get_image_data_from_url(url):
     response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
-    return img
+    return Image.open(BytesIO(response.content))
 
 def get_palette_from_data():
     palette = []
     # open data.json and get the palette
     with open('data.json', 'r') as file:
         data = json.load(file)
-        for elem in data:
-            palette.append(tuple(elem['color']))
-
+        palette.extend(tuple(elem['color']) for elem in data)
     return palette
 
 def get_coords_from_color(color):
@@ -31,5 +28,4 @@ def resize_image(image, width, height):
     ratio = min(width / original_width, height / original_height)
     new_width = int(original_width * ratio)
     new_height = int(original_height * ratio)
-    new_image = image.resize((new_width, new_height))
-    return new_image
+    return image.resize((new_width, new_height))
