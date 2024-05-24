cb6ea713815066457e3fc79408e0dd5dc2f0c098
PhilZPhaZ <bour.phileas@gmail.com>
2024-05-24 16:57:13 +0200

    speed up by adding lines

--- img.py
+++ img.py
@@ -1,3 +1,4 @@
+import itertools
 from PIL import Image
 from libs import *
 from process_image import *
@@ -32,15 +33,14 @@ class Img:
 
     def draw_pixel(self, x, y, color):
         for color_palette in self.palette:
-            if color_palette == tuple(color):
-                if tuple(color) != (255, 255, 255):
-                    self.mouse.position = get_coords_from_color(color_palette)
-                    self.click()
-
-                    self.mouse.position = (x, y)
-                    self.click()
-
-                    time.sleep(0.01)
+            if color_palette == tuple(color) and tuple(color) != (255, 255, 255):
+                self.mouse.position = get_coords_from_color(color_palette)
+                self.click()
+            
+                self.mouse.position = (x, y)
+                self.click()
+            
+                time.sleep(0.01)
     
     def start_drawing_speed(self, x_start, y_start):
         self.mouse = Controller()
@@ -51,29 +51,82 @@ class Img:
             self.mouse.position = get_coords_from_color(color_palette)
             self.click()
 
-            self.lines(x_start, y_start, x_start, y_start + 20)
-
             for x in range(image_array.shape[0]):
                 for y in range(image_array.shape[1]):
                     color = image_array[x, y]
-                    if color_palette == tuple(color):
-                        if tuple(color) != (255, 255, 255):
-                            self.mouse.position = (5 * x + x_start, 5 * y + y_start)
-                            self.click()
+                    if color_palette == tuple(color) and tuple(color) != (255, 255, 255):
+                        self.mouse.position = (5 * x + x_start, 5 * y + y_start)
+                        self.click()
 
-                            time.sleep(0.001)
-    
+                        time.sleep(0.001)
+
+
     def start_drawing_speed_lines(self, x_start, y_start):
         # same for start_drawing_speed but this time, when there are lines of the same color, draw a line and not a series of points
         self.mouse = Controller()
-        
+
         image_array = np.array(self.img)
 
+        self.heigh = image_array.shape[0]
+
+        for color_palette in self.palette:
+            self.mouse.position = get_coords_from_color(color_palette)
+            self.click()
+
+            for index, start_pixel in self.lines.items():
+                if color_palette == start_pixel[2] and start_pixel[2] != (255, 255, 255):
+                    x_start_line, y_start_line = start_pixel[0], start_pixel[1]
+                    self.mouse.position = (5 * x_start_line + x_start, 5 * y_start_line + y_start)
+                    self.press()
+
+                    try:
+                        end_pixel = self.lines[index + 1]
+                        y_end_line = end_pixel[1] - 1
+
+                        relative_y = y_end_line - y_start_line
+
+                        time.sleep(0.002)
+
+                        self.move_from_relative_y(relative_y)
+                    except KeyError:
+                        relative_y = self.heigh - y_start_line
+
+                        self.move_from_relative_y(relative_y)
+
+    def move_from_relative_y(self, relative_y):
+        self.mouse.move(0, 5 * relative_y)
+
+        time.sleep(0.0001)
+
+        self.release()
 
     def click(self):
         self.mouse.click(Button.left, 1)
-    
-    def lines(self, x1, y1, x2, y2):
+
+    def press(self):
         self.mouse.press(Button.left)
-        self.mouse.move(x2 - x1, y2 - y1)
+
+    def release(self):
         self.mouse.release(Button.left)
+
+    def check_lines(self):
+        image_array = np.array(self.img)
+
+        self.lines = {}
+
+        idx = 0
+
+        for x, y in itertools.product(range(image_array.shape[0]), range(image_array.shape[1])):
+            color = tuple(image_array[x, y])
+            last_color = tuple(image_array[x, y-1])
+
+            try:
+                # check if the last color was the same
+                if color == last_color:
+                    continue
+                else:
+                    self.lines[idx] = [x, y, color]
+            except IndexError:
+                self.lines[idx] = [x, y, color]
+
+            idx += 1
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 
 def main():
-    image = Img("https://img.freepik.com/vecteurs-premium/tigre-dessin-anime-isole-blanc_29190-5158.jpg", 100, 100)
+    image = Img("https://media.discordapp.net/attachments/910960207465680959/1081664497934151760/3B3616EF-EB6C-49F1-9E2A-3E9CB299D547.jpg?ex=665197d0&is=66504650&hm=12a1f12e04e70f7c0d18aff57bd2246aeff62fb007f926babb60f46cc3c48fd7&=&format=webp&width=611&height=621", 100, 100)
 
     image.change_palette(get_palette_from_data())
 
@@ -12,6 +12,9 @@ def main():
 
     time.sleep(10)
 
-    image.start_drawing_speed(558, 353)
+    image.check_lines()
+
+    image.start_drawing_speed_lines(558, 353)
+    # image.start_drawing_speed(558, 353)
 
 main()
